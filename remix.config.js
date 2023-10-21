b0526466a18043a4ee6f2817cc29522d9616a9d1
Nibezak <kevin.nibeza@gmail.com>
2023-10-22 00:06:33 +0300

    changed the verce and package.json file

--- remix.config.js
+++ remix.config.js
@@ -1,33 +1,9 @@
-const { flatRoutes } = require('remix-flat-routes')
-
-/**
- * @type {import('@remix-run/dev/config').AppConfig}
- */
-module.exports = {
-	ignoredRouteFiles: ['**/.*'],
-	tailwind: true,
-	postcss: true,
+/** @type {import('@remix-run/dev').AppConfig} */
+export default {
+	ignoredRouteFiles: ["**/.*"],
 	// appDirectory: "app",
-  // assetsBuildDirectory: "public/build",
-  // serverBuildPath: "build/index.js",
-  // publicPath: "/build/",
-  serverBuild: {
-    entryPoint: './app/entry.server.tsx'
-  },
-	serverModuleFormat: 'cjs',
-	future: {
-		v2_dev: true,
-		v2_errorBoundary: true,
-		v2_headers: true,
-		v2_meta: true,
-		v2_normalizeFormMethod: true,
-		v2_routeConvention: true,
-	},
-	// ignore all files in routes folder to prevent
-  // default remix convention from picking up routes
-  ignoredRouteFiles: ['**/*'],
-  routes: async defineRoutes => {
-    return flatRoutes('routes', defineRoutes)
-  },
-	serverDependenciesToBundle: [/^swiper.*/, /^ssr-window.*/, /^dom7.*/, /^react-photoswipe-gallery.*/, /^photoswipe.*/],
-};
+	// assetsBuildDirectory: "public/build",
+	// publicPath: "/build/",
+	// serverBuildPath: "build/index.js",
+  };
+  
