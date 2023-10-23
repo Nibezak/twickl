286d3190c690406d09198c4fe1e764cf84180d58
Nibezak <kevin.nibeza@gmail.com>
2023-10-23 11:08:55 +0300

    removed netlify and it's files

--- remix.config.js
+++ remix.config.js
@@ -3,20 +3,7 @@ const { flatRoutes } = require('remix-flat-routes')
 /**
  * @type {import('@remix-run/dev/config').AppConfig}
  */
-const baseConfig =
-  process.env.NODE_ENV === "production"
-    ? // when running the Netify CLI or building on Netlify, we want to use
-      {
-        server: "./server.ts",
-        serverBuildPath: ".netlify/functions-internal/server.js",
-      }
-    : // otherwise support running remix dev, i.e. no custom server
-      undefined;
-
-/** @type {import('@remix-run/dev').AppConfig} */
-
 module.exports = {
-  ...baseConfig,
 	ignoredRouteFiles: ['**/.*'],
 	tailwind: true,
 	postcss: true,
@@ -44,5 +31,3 @@ module.exports = {
   },
 	serverDependenciesToBundle: [/^swiper.*/, /^ssr-window.*/, /^dom7.*/, /^react-photoswipe-gallery.*/, /^photoswipe.*/],
 };
-
-
