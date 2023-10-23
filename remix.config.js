f6d1812ba7fd54c021b262772571f0ba4940a90c
Nibezak <kevin.nibeza@gmail.com>
2023-10-23 10:39:28 +0300

    changed package manager to pnpm

--- remix.config.js
+++ remix.config.js
@@ -3,7 +3,20 @@ const { flatRoutes } = require('remix-flat-routes')
 /**
  * @type {import('@remix-run/dev/config').AppConfig}
  */
+const baseConfig =
+  process.env.NODE_ENV === "production"
+    ? // when running the Netify CLI or building on Netlify, we want to use
+      {
+        server: "./server.ts",
+        serverBuildPath: ".netlify/functions-internal/server.js",
+      }
+    : // otherwise support running remix dev, i.e. no custom server
+      undefined;
+
+/** @type {import('@remix-run/dev').AppConfig} */
+
 module.exports = {
+  ...baseConfig,
 	ignoredRouteFiles: ['**/.*'],
 	tailwind: true,
 	postcss: true,
@@ -31,3 +44,5 @@ module.exports = {
   },
 	serverDependenciesToBundle: [/^swiper.*/, /^ssr-window.*/, /^dom7.*/, /^react-photoswipe-gallery.*/, /^photoswipe.*/],
 };
+
+
